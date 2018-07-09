83e412c1ac4f471334db5a28f9b991c046df4935
DannyBoyNYC <DannyBoyNYC>
2018-07-09 14:11:20 -0400

    done

--- react-pirates-done/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base'
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyAHnKw63CUBAqSuCREgils_waYJ0qwpGiU",
-  authDomain: "daniel-deverell-pirates.firebaseapp.com",
-  databaseURL: "https://daniel-deverell-pirates.firebaseio.com",
-})
-
-export default base
