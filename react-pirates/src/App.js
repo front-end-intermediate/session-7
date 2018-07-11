0b05957fc9cb4c33159b4bc8b3cc80618f087c21
DannyBoyNYC <daniel.deverell@gmail.com>
2018-07-10 22:22:40 -0400

    done

--- react-pirates/src/App.js
+++ react-pirates/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import base from './base';
 import Header from './components/Header';
 import Pirate from './components/Pirate';
 import PirateForm from './components/PirateForm';
@@ -11,6 +12,8 @@ class App extends Component {
   constructor() {
     super();
     this.addPirate = this.addPirate.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.removePirate = this.removePirate.bind(this);
     this.state = {
       pirates: {}
     }
@@ -23,13 +26,18 @@ class App extends Component {
         <ul>
           {
             Object.keys(this.state.pirates)
-              .map(key => <Pirate
+              .map( key => <Pirate
                 key={key}
-                details = {this.state.pirates[key]}
+                index={key}
+                details={this.state.pirates[key]}
+                removePirate = {this.removePirate}
               />)
           }
         </ul>
-        <PirateForm addPirate={this.addPirate} />
+        <PirateForm
+          addPirate={this.addPirate}
+          loadSamples={this.loadSamples}
+        />
       </div>
     );
   }
@@ -39,10 +47,33 @@ class App extends Component {
     const pirates = {...this.state.pirates}
     //create a unique id
     const timestamp = Date.now()
-    //add new pirate using accessor and id - objectName["propertyName"] and assignment
+    //add new pirate using accessor and id - 
+    // objectName["propertyName"] and assignment
     pirates[`pirate-${timestamp}`] = pirate
     //set state pirates with var pirates
     this.setState({ pirates: pirates })
+  }
+
+  loadSamples() {
+    this.setState({pirates: piratesFile})
+  }
+
+  removePirate(key) {
+    const pirates = { ...this.state.pirates }
+    // delete pirates[key]
+    pirates[key] = null;
+    this.setState({pirates})
+  }
+
+  componentWillMount(){
+    this.ref = base.syncState(`daniel-deverell-pirates/pirates`, {
+      context: this,
+      state: 'pirates'
+    })
+  }
+
+  componentWillUmount(){
+    base.removeBinding(this.ref)
   }
 
 }
